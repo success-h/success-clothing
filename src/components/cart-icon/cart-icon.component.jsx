b7ac8bc8ec0157f0d9d3cebf049223f29a42a64a
success-h <successhycenth8@gmail.com>
2021-03-07 23:44:37 +0100

    updating our appication with reselect, replaced cart selectors with mwmorize selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -9,18 +9,21 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
-  <div className='cart-icon' onClick={toggleCartHidden}>
-    <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>{itemCount}</span>
-  </div>
+	<div className='cart-icon' onClick={toggleCartHidden}>
+		<ShoppingIcon className='shopping-icon' />
+		<span className='item-count'>{itemCount}</span>
+	</div>
 );
 
 const mapDispatchToProps = (dispatch) => ({
-  toggleCartHidden: () => dispatch(toggleCartHidden()),
+	toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
+x - special / nautilus - clipboard;
+cut;
+///home/success/Videos/react%20playground/ecomance%20app/success-clothing
 
-const mapStateToProps = (state) => ({
-  itemCount: selectCartItemsCount(state),
+file: const mapStateToProps = (state) => ({
+	itemCount: selectCartItemsCount(state),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -11,35 +11,35 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
 const Header = ({ currentUser, hidden }) => (
-  <div className='header'>
-    <Link className='logo-container' to='/'>
-      <Logo className='logo' />
-    </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
-        SHOP
-      </Link>
-      <Link className='option' to='/shop'>
-        CONTACT
-      </Link>
-      {currentUser ? (
-        <div className='option' onClick={() => auth.signOut()}>
-          SIGN OUT
-        </div>
-      ) : (
-        <Link className='option' to='/signin'>
-          SIGN IN
-        </Link>
-      )}
-      <CartIcon />
-    </div>
-    {hidden ? null : <CartDropdown />}
-  </div>
+	<div className='header'>
+		<Link className='logo-container' to='/'>
+			<Logo className='logo' />
+		</Link>
+		<div className='options'>
+			<Link className='option' to='/shop'>
+				SHOP
+			</Link>
+			<Link className='option' to='/shop'>
+				CONTACT
+			</Link>
+			{currentUser ? (
+				<div className='option' onClick={() => auth.signOut()}>
+					SIGN OUT
+				</div>
+			) : (
+				<Link className='option' to='/signin'>
+					SIGN IN
+				</Link>
+			)}
+			<CartIcon />
+		</div>
+		{hidden ? null : <CartDropdown />}
+	</div>
 );
 
 const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-  currentUser,
-  hidden,
+	currentUser,
+	hidden,
 });
 
 export default connect(mapStateToProps)(Header);
