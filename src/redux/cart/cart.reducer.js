da3c7b8f53b6aa5d35d642403914b513e2897043
success-h <successhycenth8@gmail.com>
2021-03-09 11:10:01 +0100

    added our clear item and updated our state

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,26 +1,34 @@
+import { clearItemFromCart } from './cart.actions';
 import CartActionTypes from './cart.types';
 import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
-  hidden: true,
-  cartItems: []
+	hidden: true,
+	cartItems: [],
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case CartActionTypes.TOGGLE_CART_HIDDEN:
-      return {
-        ...state,
-        hidden: !state.hidden
-      };
-    case CartActionTypes.ADD_ITEM:
-      return {
-        ...state,
-        cartItems: addItemToCart(state.cartItems, action.payload)
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case CartActionTypes.TOGGLE_CART_HIDDEN:
+			return {
+				...state,
+				hidden: !state.hidden,
+			};
+		case CartActionTypes.ADD_ITEM:
+			return {
+				...state,
+				cartItems: addItemToCart(state.cartItems, action.payload),
+			};
+		case CartActionTypes.CLEAR_ITEM_FROM_CART:
+			return {
+				...state,
+				cartItems: state.cartItems.filter(
+					(cartItem) => cartItem.id !== action.payload.id
+				),
+			};
+		default:
+			return state;
+	}
 };
 
 export default cartReducer;
